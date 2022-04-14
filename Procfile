48025002d531c612df56ba6e1363e4faccf1bc6b
Sinmiloluwa <mofeoluwae@gmail.com>
2022-04-14 23:43:49 +0000

    Added procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-apache2 public/
+web: web: node app.js
--- app.js
+++ app.js
@@ -18,8 +18,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
+const PORT = process.env.PORT || 3000;
 if(connectDb) {
-    app.listen(3000)
+    app.listen(PORT)
     console.log("Listening on Port")
 }
 
