576617ddab6b80096e0ec3976a8e78c7518d017f
Sinmiloluwa <mofeoluwae@gmail.com>
2022-04-15 20:43:00 +0000

    Modified Package json

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // connect to db
 module.exports = {
-    connectDb : mongoose.connect('mongodb://localhost:27017/streaming', {useNewUrlParser : true, useUnifiedTopology: true})
+    connectDb : mongoose.connect("mongodb+srv://Simons:Precious97!@cluster0.ci0ov.mongodb.net/vodeo?retryWrites=true&w=majority")
     .then((result) => console.log('Connected'))
     .catch((err) => console.log(err))
 }
