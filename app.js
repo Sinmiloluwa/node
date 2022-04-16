f2152a202405ede6ec32589e484a10740c578955
Sinmiloluwa <mofeoluwae@gmail.com>
2022-04-16 16:45:59 +0000

    Modified Package json

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(cookieParser())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 
 
