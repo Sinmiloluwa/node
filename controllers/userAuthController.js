4094907e0d8dbaf6e97f0c245cc4be2aafa20965
Sinmiloluwa <mofeoluwae@gmail.com>
2022-04-17 13:25:01 +0000

    Cors

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -36,8 +36,7 @@ const user_signup = async (req, res) => {
     try{
         const user = await User.create({email, password, username})
         const token = createToken(user._id)
-        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
-        res.status(201).json({user: user._id})
+        res.status(201).json({user: user._id, token: token})
     }
     catch(err) {
         const errors = handleErrors(err)
