734ec362b29dcdeb881a78de59699ae4e6c075d3
Sinmiloluwa <mofeoluwae@gmail.com>
2022-04-17 19:49:34 +0000

    User Login

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -49,7 +49,7 @@ const user_signin = async (req, res) => {
 
     try {
         const user = await User.login(email, password)
-        res.status(200).json({id : user._id})
+        res.status(200).json({user : user._id})
     }
     catch(err) {
         res.status(400).json({})
